5b7dc59e9dcb4fcaa94c81419e6c5ba13fa3eef6
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-03-20 11:38:27 +0800

    init

---
+++ bin/ridesafe_bluetooth_impl.dart
@@ -0,0 +1,5 @@
+import 'package:ridesafe_bluetooth_impl/ridesafe_bluetooth_impl.dart' as ridesafe_bluetooth_impl;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ridesafe_bluetooth_impl.calculate()}!');
+}
---
+++ test/ridesafe_bluetooth_impl_test.dart
@@ -0,0 +1,8 @@
+import 'package:ridesafe_bluetooth_impl/ridesafe_bluetooth_impl.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
