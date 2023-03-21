0517cceefc71a6d4a38a2ee4d5fd8c9c5107f221
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-03-21 23:02:21 +0800

    refactor bt service

--- lib/service/bluetooth_service.dart
+++ lib/service/bluetooth_service.dart
@@ -2,12 +2,7 @@ import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:ridesafe_bluetooth_impl/repositories/bluetooth_service_interactor.dart';
 
 class BluetoothService {
-  final BluetoothServiceInteractor _interactor;
-
-  BluetoothService()
-      : _interactor = BluetoothServiceInteractor(
-          FlutterBluetoothSerial.instance,
-        );
-
-  BluetoothServiceInteractor get service => _interactor;
+  static BluetoothServiceInteractor get service => BluetoothServiceInteractor(
+        FlutterBluetoothSerial.instance,
+      );
 }
